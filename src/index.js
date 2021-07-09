5dd99fa026e41f22b8bb4b8fa9b5db6bfbac07a5
Vitali Hauzinski <hauzinski@gmail.com>
2021-07-09 15:32:30 +0300

    init: typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+exports.min = function min(array) {
+    if (!array || !array.length) {
+        return 0;
+    }
+    let count = array[0];
+    for (i = 0; i < array.length; i++) {
+        if (array[i] < count) {
+            count = array[i];
+        }
+    }
+    return count;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || !array.length) {
+        return 0;
+    }
+    let count = array[0];
+    for (i = 0; i < array.length; i++) {
+        if (array[i] > count) {
+            count = array[i];
+        }
+    }
+    return count;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || !array.length) {
+        return 0;
+    }
+    let sum = 0;
+    let count = array.length;
+    for (i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / count;
+};
